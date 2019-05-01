c8ab0eeb5227a479bb8c62c7ab34b1347596d9a8
Marc <marcsanuy@gmail.com>
2019-05-01 12:26:43 +0200

    animations

--- js/script.js
+++ js/script.js
@@ -5,19 +5,17 @@
      $(window).scroll(function(){
          scroll = $(window).scrollTop();
 
-         if ($(window).widht() > 480) {
+         if ($(window).width() > 480) {
             if (scroll > 100) {
-                 $(".menu").css({"background":"#fff"});
+                 $(".menu").css({"background":"#737373"});
                  $(".menu").css({"padding":"5px 0"});
                  $(".menu").addClass("shadow");
-                $(".nav ul li a").css({"color":"#30374b"});
-                $(".logo").css({"color":"#000"});
+                 
             } else {
                 $(".menu").css({"background":"transparent"});
                 $(".menu").css({"padding":"20px 0"});
                  $(".menu").removeClass("shadow");
-                 $(".nav ul li a").css({"color":"#fff"});
-                 $(".logo").css({"color":"#fff"});
+                 
              }
          }
      })
